70d2450ff1fbac2d856349a0d667657852886ebc
bas10agu <alfredbastin.agustines04@gmail.com>
2026-09-03 00:08:37 +0800

    Updated previous stats issues and created PlayerBattleStats

---
+++ Custom Resources/player_battle_stats.gd
@@ -0,0 +1,27 @@
+class_name PlayerBattleStats
+extends Stats
+
+@export var starting_deck: CardPile
+@export var cards_per_turn: int
+
+var deck: CardPile
+var discard: CardPile
+var draw_pile: CardPile
+var combo: int = 0 : set = set_combo
+
+func set_combo(value: int) -> void:
+	combo = value
+	stats_changed.emit()
+
+func reset_combo() -> void:
+	self.combo = 0
+
+func create_instance() -> Resource:
+	var instance: PlayerBattleStats = self.duplicate()
+	instance.health = max_health
+	instance.block = blockState.NONE
+	instance.reset_combo()
+	instance.deck = instance.starting_deck.duplicate()
+	instance.discard = CardPile.new()
+	instance.draw_pile = CardPile.new()
+	return instance
--- Custom Resources/stats.gd
+++ Custom Resources/stats.gd
@@ -6,11 +6,11 @@ signal stats_changed
 enum blockState {NONE, FULL, HALF}
 
 @export var max_health := 1
-@export var art: Texture
+@export var base_armor := 0
 
 var health: int : set = set_health
 var block: blockState = blockState.NONE : set = set_block
-
+var armor: int = 0 : set = set_armor
 
 func set_health(value: int) -> void:
 	health = clampi(value, 0, max_health)
@@ -20,14 +20,19 @@ func set_block(value: blockState) -> void:
 	block = value
 	stats_changed.emit()
 
+func set_armor(value: int) -> void:
+	armor = maxi(0, value)
+	stats_changed.emit()
+
 func take_damage(damage: int) -> void:
+	var mitigated_damage: int = maxi(0, damage - self.armor)
+	
 	if damage <= 0 or block == blockState.FULL:
 		return
-	
-	if block == blockState.NONE:
-		self.health -= damage
+	elif block == blockState.NONE:
+		self.health -= mitigated_damage
 	elif block == blockState.HALF:
-		self.health = (damage / 2)
+		self.health -= (mitigated_damage / 2)
 
 func heal(amount: int) -> void:
 	self.health += amount
@@ -36,4 +41,5 @@ func create_instance() -> Resource:
 	var instance: Stats = self.duplicate()
 	instance.health = max_health
 	instance.block = blockState.NONE
+	instance.armor = base_armor
 	return instance
