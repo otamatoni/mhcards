edf496f595ca36485262a1e99e545e406a03b12f
otamatoni <137796211+otamatoni@users.noreply.github.com>
2026-09-05 15:22:04 +0800

    remove untargetable priority, will replace w status

--- Scripts/Monsters/BigMonsters/Jyuratodus.gd
+++ Scripts/Monsters/BigMonsters/Jyuratodus.gd
@@ -5,11 +5,10 @@ class_name Jyuratodus
 var dive_action = Action.new(dive)
 var jump_action = Action.new(jump)
 var water_spit_action = Action.new(water_spit)
-var underwater_action = Action.new(underwater, BattleVariables.untargetable)
 
 func _ready() -> void:
 	super()
-	sequences.append(Sequence.new([dive_action, underwater_action, jump_action]))
+	sequences.append(Sequence.new([dive_action, wait_action, jump_action]))
 	sequences.append(Sequence.new([water_spit_action]))
 	sequences.append(Sequence.new([water_spit_action, water_spit_action]))
 	sequences.append(Sequence.new([water_spit_action, water_spit_action, water_spit_action]))
@@ -25,6 +24,3 @@ func jump() -> void:
 
 func water_spit() -> void:
 	print('jyura spits water')
-	
-func underwater() -> void:
-	print('jyura is underwater')
--- Scripts/Monsters/BossMonsters/Diablos.gd
+++ Scripts/Monsters/BossMonsters/Diablos.gd
@@ -4,14 +4,13 @@ class_name Diablos
 
 var dig_action = Action.new(dig)
 var jump_action = Action.new(jump)
-var underground_action = Action.new(underground, BattleVariables.untargetable)
 var ready_charge_action = Action.new(ready_charge)
 var charge_action = Action.new(charge)
 
 func _ready() -> void:
 	super()
 	sequences.append(Sequence.new([roar_action, attack_action, attack_action]))
-	sequences.append(Sequence.new([dig_action, underground_action, jump_action]))
+	sequences.append(Sequence.new([dig_action, wait_action, jump_action]))
 	sequences.append(Sequence.new([ready_charge_action]))
 	sequences.append(Sequence.new([charge_action, charge_action, charge_action]))
 	
@@ -22,9 +21,6 @@ func dig() -> void:
 	
 func jump() -> void:
 	print('diablos emerges')
-	
-func underground() -> void:
-	print('diablos is underground')
 	
 func ready_charge() -> void:
 	print('diablos paws at the ground')
